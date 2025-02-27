139ab80e16cab007cf40ab81cdecf180d503b388
Aryan75Way <aryn.75way@gmail.com>
2025-02-27 15:24:57 +0530

    create: initial frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <button className="flex items-center justify-center text-2xl w-full h-screen"><div>ChatSy</div></button>
+    </>
+  )
+}
+
+export default App
